1b1b0465fc5f33dba96c83b329d678497086a281
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-09-30 00:11:56 -0300

    Creating Routes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 //setup crossroads
-crossroads.addRoute('foo');
-crossroads.addRoute('lorem/ipsum');
-crossroads.routed.add(console.log, console); //log all routes
+crossroads.addRoute('/', function(){
+	console.log('Hello!')
+});
+
+crossroads.addRoute('/products', function(){
+	console.log('Bye!')
+});
+
+crossroads.addRoute('/contact', function(){
+	console.log('Bye!')
+});
 
 //setup hasher
 function parseHash(newHash, oldHash){
@@ -10,6 +18,3 @@ function parseHash(newHash, oldHash){
 hasher.initialized.add(parseHash); //parse initial hash
 hasher.changed.add(parseHash); //parse hash changes
 hasher.init(); //start listening for history change
-
-//update URL fragment generating new history record
-hasher.setHash('lorem/ipsum');
